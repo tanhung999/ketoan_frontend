64c22383f16a47b124e6513fbe2cf0caeb39943a
tanhung999 <ngotanhung02011999@gmail.com>
2023-12-08 13:37:02 +0700

    add icon in app_bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box'
 import AppsIcon from '@mui/icons-material/Apps'
 import ModeSelect from '../ModeSelect'
-import { Typography } from '@mui/material'
+import Typography from '@mui/material/Typography'
 import Categories from './Menus/Categories'
 import Document from './Menus/Document'
 import Profiles from './Menus/Profiles'
@@ -13,6 +13,14 @@ import Badge from '@mui/material/Badge'
 import Tooltip from '@mui/material/Tooltip'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
+import AddToPhotosIcon from '@mui/icons-material/AddToPhotos'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import SearchIcon from '@mui/icons-material/Search'
+import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined'
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
+import MoreOutlinedIcon from '@mui/icons-material/MoreOutlined'
 // import { ReactComponent as AccountantIcon } from '~/assets/accounting-icon.svg'
 // import SvgIcon from '@mui/material/SvgIcon'
 function AppBar () {
@@ -35,7 +43,8 @@ function AppBar () {
     >
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+          <AccountBalanceIcon variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}/>
           {/* <SvgIcon component={AccountantIcon} inheritViewBox sx={{ color: 'primary.main' }} fontSize="small" /> */}
           <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Accountant</Typography>
         </Box>
@@ -44,13 +53,42 @@ function AppBar () {
           <Document />
           <GoodsReceipt />
           <GoodsIssueNote />
-          <Button variant='outlined' >Bảng Cân Đối</Button>
+          <Button variant='outlined' startIcon={ <AddToPhotosIcon />}>Bảng Cân Đối</Button>
         </Box>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
-        <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: 120 }}/>
-        <ModeSelect />
+        <TextField
+          id="outlined-search"
+          label="Search..."
+          type="search"
+          size='small'
+          sx={{
+            minWidth: 120,
+            '& .MuiOutlinedInput-root': {
+              paddingRight: 0
+            }
+          }}
 
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        <ModeSelect />
+        <Tooltip title="More Infomation">
+          <MoreOutlinedIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
+        </Tooltip>
+        <Tooltip title="Settings Application">
+          <SettingsOutlinedIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
+        </Tooltip>
+        <Tooltip title="Message">
+          <CommentOutlinedIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
+        </Tooltip>
         <Tooltip title="Notification">
           <Badge color="secondary" variant='dot' sx={{ cursor: 'pointer' }}>
             <NotificationsNoneIcon sx={{ color: 'primary.main' }}/>
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,65 @@
 import Box from '@mui/material/Box'
-
+import Chip from '@mui/material/Chip'
+import FaceIcon from '@mui/icons-material/Face'
 function NavBar () {
+  const MENU_STYLES = {
+    color: 'primary.main',
+    bgcolor: 'white',
+    border: 'none',
+    paddingX: '5px',
+    borderRadius: '4px',
+    '& .MuiSvgIcon-root': {
+      color: 'primary.main'
+    },
+    '&:hover':{
+      bgcolor: 'primary.50'
+    }
+  }
   return (
-    <>
-      <Box
-        sx ={{
-          backgroundColor: 'primary.dark',
-          width: '100%',
-          height:(theme) => theme.appCustom.navBarHeight,
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-        Nav Bar
+    <Box
+      sx ={{
+        // backgroundColor: 'primary.dark',
+        width: '100%',
+        height:(theme) => theme.appCustom.navBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        overflowX: 'auto',
+        borderTop: '2px solid #00bfa5',
+        paddingX: 2
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          icon={<FaceIcon />}
+          label="With Icon"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<FaceIcon />} l
+          label="With Icon"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<FaceIcon />}
+          label="With Icon"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<FaceIcon />}
+          label="With Icon"
+          clickable
+          sx={MENU_STYLES}
+        />
       </Box>
-    </>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+
+      </Box>
+    </Box>
   )
 }
 export default NavBar
